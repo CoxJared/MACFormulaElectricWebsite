6731db2a0cff17cade09d7b376ab1e431ed3f7ca
Jared Cox <coxj1@mcmaster.ca>
2020-06-25 21:55:23 -0400

    mobile design showcase

--- src/components/designPage/DesignPage.jsx
+++ src/components/designPage/DesignPage.jsx
@@ -27,16 +27,51 @@ export class DesignPage extends Component {
 
   render() {
 
-    return (
-      <div className="cars-container">
-        <Header />
-        <MainShowcase scrollPosition={this.state.scrollPosition}/>
-        <CrossView />
-        <DesignShowcase testfunction={this.test}/>
-        
-        <Footer />
-      </div>
-    )
+      if(window.innerWidth > 1200){
+        return (
+          <div className="cars-container">
+            <Header />
+            <MainShowcase scrollPosition={this.state.scrollPosition}/>
+            <CrossView />
+            <DesignShowcase testfunction={this.test}/>
+            
+            <Footer />
+          </div>
+        )
+      } else {
+        return (
+          <div className="cars-container">
+            <Header />
+            <div className="minimized-design-showcase">
+              <div className="minimized-design-image-container">
+                <div className="minimized-image-cut"></div>
+                <div id="minimized-image-full"></div>
+              </div>
+              <h1 className="minimized-car-name">ZAPDOS</h1>
+            </div>
+              <div id="minimized-main-specs">
+            <div id="minimized-spec">
+              <h1 className="minimized-spec-title">Power</h1>
+              <h2 className="minimized-spec-value">107hp<span>/80kW</span></h2>
+            </div>
+            <div id="minimized-spec">
+              <h1 className="minimized-spec-title">Weight</h1>
+              <h2 className="minimized-spec-value">220kg<span>/484lb</span></h2>
+            </div>
+            <div id="minimized-spec">
+              <h1 className="minimized-spec-title">Capacity</h1>
+              <h2 className="minimized-spec-value">6.6kWh<span></span></h2>
+            </div>
+            <div id="minimized-spec">
+              <h1 className="minimized-spec-title">Drivetrain</h1>
+              <h2 className="minimized-spec-value">AWD<span></span></h2>
+            </div>
+          </div>
+            <CrossView />
+            <Footer />
+          </div>
+        )
+      }
   }
 }
 
