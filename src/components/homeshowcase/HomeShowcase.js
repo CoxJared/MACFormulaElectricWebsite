5b88452809638217170cc096d4ae50eca1187370
Jared Cox <coxj1@mcmaster.ca>
2020-06-27 22:02:26 -0400

    header fixes

--- src/components/homeshowcase/HomeShowcase.js
+++ src/components/homeshowcase/HomeShowcase.js
@@ -20,6 +20,7 @@ export class HomeShowcase extends Component {
         
           componentDidMount() {
             window.scrollTo(0, 0)
+            this.initialLoad()
           } 
 
           imageDidLoad() {
@@ -27,6 +28,7 @@ export class HomeShowcase extends Component {
               this.setState({loaded: true})
             }
           }
+
           initialLoad() {
             this.updateElements()
             this.moveArrow()
@@ -36,38 +38,41 @@ export class HomeShowcase extends Component {
             setTimeout(
                 function(){
                   try{
-                  //text appear
-                  (document.getElementById("landing-title-container")).style.opacity="1";
-                  (document.getElementById("landing-title-container")).style.top="360px";
-                  (document.getElementById("landing-subtitle")).style.marginTop="0px";
-                  (document.getElementById("landing-image-container")).style.left= '0px';
+                    //text appear
+                    (document.getElementById("landing-title-container")).style.opacity="1";
+                    (document.getElementById("landing-title-container")).style.top="360px";
+                    (document.getElementById("landing-subtitle")).style.marginTop="0px";
+                    (document.getElementById("landing-image-container")).style.left= '0px';
 
-                  //image fade in
-                  (document.getElementById("landing-image-container")).style.opacity="1";
-                  }catch{}
+                    //image fade in
+                    (document.getElementById("landing-image-container")).style.opacity="1";
+                  } catch {}
                 }, 500)
           }
         
           moveArrow() { 
             setTimeout(
-            function(){
-              try{
-              document.getElementById("enter-arrow").style.top="50px"
-              document.getElementById("enter-arrow").style.opacity="1"
-              setTimeout(
-                  function(){
-                    try{
-                      document.getElementById("enter-arrow").style.opacity="0";
-                    }catch{}
-                  },1000)
-                  }catch{}
-                  },4000)
+              function(){
+                try {
+                  document.getElementById("enter-arrow").style.top="50px"
+                  document.getElementById("enter-arrow").style.opacity="1"
+                  setTimeout(
+                      function(){
+                        try {
+                          document.getElementById("enter-arrow").style.opacity="0";
+                        } catch {}
+                      },1000)
+                    } catch {}
+                },4000)
                 
           }
         
           updateShowcase = () => {
             (document.getElementById("landing-image-dark")).style.opacity= 0;
             (document.getElementById("landing-title-container")).style.opacity= 0;
+            (document.getElementById("landing-image-container")).style.marginTop="00px";
+
+
 
             if(window.innerWidth < 750 ){
               (document.getElementById("landing-title-container")).style.top= '-20vw';
@@ -75,16 +80,14 @@ export class HomeShowcase extends Component {
               (document.getElementById("landing-title-container")).style.top= '00px';
             }
             
-            (document.getElementById("landing-image-container")).style.marginTop="00px";
-            // (document.getElementById("landing-image-container")).style.paddingLeft="100px";
-
             if(window.innerWidth > 1200){
-            (document.getElementById("landing-image-container")).style.maxWidth="780px";
-            (document.getElementById("landing-image-container")).style.left= '280px';
+              (document.getElementById("landing-image-container")).style.maxWidth="780px";
+              (document.getElementById("landing-image-container")).style.left= '280px';
             } else {
               (document.getElementById("landing-image-container")).style.marginTop="5vw";
               (document.getElementById("landing-image-container")).style.maxWidth="60vw";
-            (document.getElementById("landing-image-container")).style.left= '23vw';
+              (document.getElementById("landing-image-container")).style.left= '23vw';
+              console.log('yep')
             }
             this.props.DidEnter()
             this.setState({entered:true})
@@ -119,42 +122,41 @@ export class HomeShowcase extends Component {
           }
         
           render() {
-            if(this.state.loaded && !this.state.entered) {
-              this.initialLoad();
-            }
+            // if(this.state.loaded && !this.state.entered) {
+            //   this.initialLoad();
+            // }
 
             if(window.innerWidth > 750){
             this.checkScrolling()
             return (
               <div className="landing-ether">
-              <div id="landing-page-container">
-                <div id="landing-image-container">
-                  <img id="landing-image" src={lightImage} alt=""/>
-                  <img id="landing-image-dark" src={darkImage} alt="" onLoad={this.imageDidLoad()}/>
+                <div id="landing-page-container">
+                  <div id="landing-image-container">
+                    <img id="landing-image" src={lightImage} alt=""/>
+                    <img id="landing-image-dark" src={darkImage} alt="" onLoad={this.imageDidLoad()}/>
+                  </div>
+                  <div id="landing-title-container">
+                    <h1 className="landing-title">MAC Formula Electric</h1>
+                    <h2 id="landing-subtitle">Inspiring a Culture of Innovation</h2>
+                  </div>
+                  <div className="enter-arrow-container" onClick={this.moveLanding}>
+                    <img id="enter-arrow" src={arrow} alt=""></img>
+                  </div>
+                  <PoweredBy show={this.state.entered} moveup={this.props.didPassShowcase}/>
+                <InstagramShowcase show={this.state.entered} moveup={this.props.didPassShowcase}/>
                 </div>
-                <div id="landing-title-container">
-                  <h1 className="landing-title">MAC Formula Electric</h1>
-                  <h2 id="landing-subtitle">Inspiring a Culture of Innovation</h2>
-                </div>
-                <div className="enter-arrow-container" onClick={this.moveLanding}>
-                  <img id="enter-arrow" src={arrow} alt=""></img>
-                </div>
-                <PoweredBy show={this.state.entered} moveup={this.props.didPassShowcase}/>
-              <InstagramShowcase show={this.state.entered} moveup={this.props.didPassShowcase}/>
-              </div>
-              
               </div>
             )
-          } else {
-            return(
-            <div className="landing-mobile">
-              <img className="mobile-showcase-pic" src={darkImage} alt=""/>
-              <div id="landing-title-container">
-                  <h1 className="landing-title">MAC Formula Electric</h1>
-                  <h2 id="landing-subtitle">Inspiring a Culture of Innovation</h2>
-                </div>
-            </div>
-            )}
+            } else {
+              return(
+              <div className="landing-mobile">
+                <img className="mobile-showcase-pic" src={darkImage} alt=""/>
+                <div id="landing-title-container">
+                    <h1 className="landing-title">MAC Formula Electric</h1>
+                    <h2 id="landing-subtitle">Inspiring a Culture of Innovation</h2>
+                  </div>
+              </div>
+              )}
         }
 }
 
