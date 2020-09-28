4e0f319e1e62725981ebfe14485f35df01060d16
Jared Cox <coxj1@mcmaster.ca>
2020-09-27 23:04:05 -0400

    typo

---
+++ src/components/recruitement/recruitmentinfo/WhyJoin.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './WhyJoin.css';
+
+export class WhyJoin extends Component {
+    render() {
+        return (
+            <div className="why-join-container">
+                <div className="showbox">
+                    <div className="why-join-intro-paragraph-container">
+                        <p className="why-join-intro-paragraph">
+                            MAC Formula Electric is always interested in
+                            enthusiastic young engineers who are looking to
+                            apply their knowledge to a real world engineering
+                            problem.
+                        </p>
+                    </div>
+                    <div className="why-join-intro-paragraph-container">
+                        <p className="why-join-intro-paragraph">
+                            By joining MAC Formula Electric you will be shown
+                            processes and manufacturing techniques that you may
+                            not have otherwise had a chance to see. This
+                            includes material analyses and production of
+                            composite components, standard processes used
+                            elsewhere in aviation and aerospace. Custom Circuit
+                            Boards designed, assembled and tested in house.
+                            Design and testing of a CAN bus communications
+                            network used heavily in the automotive industry. And
+                            a 600V, 138 lithium-ion cell Accumulator developed
+                            and assembled by our team. In addition, you will be
+                            meeting representatives from a variety of industries
+                            interested in the exact skills you will be gaining.
+                            Feel free to apply to as many different positions as
+                            you like!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default WhyJoin;
